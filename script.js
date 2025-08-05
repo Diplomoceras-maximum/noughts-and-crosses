68a3623b497a0bfc4369e8add00f4daf09667342
Max <214122627+Diplomoceras-maximum@users.noreply.github.com>
2025-08-05 17:32:22 +0100

    Add planning and requirements to be met comments

---
+++ script.js
@@ -0,0 +1,20 @@
+// ##################################################
+// Planning
+// ##################################################
+
+// === Noughts and Crosses ===
+// 1. Two players take turns marking spaces in a three-by-three grid
+// 2. One player with Xs and the other with Os
+// 3. A player wins when they mark all three spaces in a row, column, or diagonal
+// 4. If no player has a line of three marks, the game is a tie
+
+// === Requirements ===
+// 1. Two player game that allows them to enter their names
+// 2. Start and Reset button
+// 3. Display that shows the result of the game
+// ##################################################
+// 4. Logic that checks for when a game is over
+// 5. Logic that checks for all winning 3-in-a-rows and ties
+// 6. Function that renders the contents of the gameboard array to page (grid)
+// 7. Functions that allow players to add marks to specific areas on the board by clicking the grid cells
+// 8. Logic that stops players from marking spots that are already taken
